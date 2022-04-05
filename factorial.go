6393ff738953b3230d7f22f1b18eda6a082629a0
williamneo <williamneokh@gmail.com>
2022-04-05 12:25:10 +0800

    done activity

--- factorial.go
+++ factorial.go
@@ -1,7 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 //Declare factorial function here
+func factorial(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * factorial(n-1)
+}
 
 func main() {
 	//Insert Code here
+	var factorialVal string
+	fmt.Println("Input a value")
+	fmt.Scanln(&factorialVal)
+
+	factorialValue, _ := strconv.ParseInt(factorialVal, 10, 0)
+
+	result := factorial(int(factorialValue))
+	fmt.Println(result)
+
 }
